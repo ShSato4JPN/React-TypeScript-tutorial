a7b3503d639886c481d54e6fd060f778eef443be
ShSato4 <shsato4@ShSato4noMacBook-Air.local>
2022-07-09 00:56:23 +0900

    first commit

---
+++ typescript-tutorial/src/App.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { isDefaultClause } from 'typescript'
+import { isDataView } from 'util/types'
+import './App.css'
+import InputField from './components/InputField'
+import { Todo } from './model'
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>('')
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault()
+    if (todo) {
+      // ロジックの説明
+      //　[...todos, { object }] で既に登録されているデータの末尾に新たデータを登録している。
+      //　。..todos　⇨　既に登録されているデータをスプレッドで抜き出し
+      //　{object} ⇨　新たなオブジェクトを登録する
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }])
+      setTodo('')
+    }
+  }
+
+  console.log(todos)
+
+  return (
+    <div className="App">
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {/* <todoList /> */}
+    </div>
+  )
+}
+
+export default App
